aee43ae0d52540cd024af060773ee1ac3e72d4b5
csh1man <tjgusyt@gmail.com>
2023-09-22 10:57:12 +0900

    hoxy project start

---
+++ src/main/java/com/community/hoxy/HoxyApplication.java
@@ -0,0 +1,13 @@
+package com.community.hoxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/hoxy/HoxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.hoxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
